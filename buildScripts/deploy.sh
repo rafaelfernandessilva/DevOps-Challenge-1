6bd3d39f1fc21277bcf136003cfb387d53f00520
Keshav-Uipl-at-645189432027 <keshavt@umbrellainfocare.com>
2020-08-19 19:07:34 +0530

    updated deploy.sh

--- buildScripts/deploy.sh
+++ buildScripts/deploy.sh
@@ -8,6 +8,6 @@ TAG=$COMMIT_ID-$BUILD_NUMBER
 echo $TAG
 
 cat docker-compose.yml
-sed -i "s/python-test:.*/python-test:"$TAG"/" docker-compose.yml
+sed -i "s/python-test:TAG/python-test:"$TAG"/" docker-compose.yml
 
 #docker-compose up -d --build
